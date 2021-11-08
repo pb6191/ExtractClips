b45d44dad5e1243280d475c9ba775344bd25d4a0
Puneet Bhargava <pbhargava936@gmail.com>
2021-11-07 22:23:38 -0800

    fix path

--- pngGen.py
+++ pngGen.py
@@ -22,7 +22,7 @@ chrome_options.add_argument("--headless")
 
 chrome_path = "./chromedriverLINUX"
 os.chmod(chrome_path, 0o755)
-subprocess.Popen(chrome_path)
+subprocess.Popen([r"./chromedriverLINUX"])
 driver = webdriver.Chrome(executable_path=chrome_path, options=chrome_options)
 x = 5120
 y = x/16*10
