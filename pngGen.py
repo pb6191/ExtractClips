37dc189c992134329497721d3fe68c0950178e49
Puneet Bhargava <pbhargava936@gmail.com>
2021-11-07 22:44:00 -0800

    no sudo need

--- pngGen.py
+++ pngGen.py
@@ -22,10 +22,12 @@ chrome_options.add_argument("--headless")
 
 chrome_path = "./chromedriverLINUX"
 os.chmod(chrome_path, 0o755)
-os.system('sudo apt-get install -y libglib2.0-0=2.50.3-2 \
+os.system('apt-get install -y libglib2.0-0=2.50.3-2 \
     libnss3=2:3.26.2-1.1+deb9u1 \
     libgconf-2-4=3.2.6-4+b1 \
-    libfontconfig1=2.11.0-6.7+b1')
+    libfontconfig1=2.11.0-6.7+b1 \
+    libgtk2.0-0:i386 \
+    libsm6:i386')
 subprocess.Popen([r"./chromedriverLINUX"])
 #executable_path=chrome_path, 
 driver = webdriver.Chrome(options=chrome_options)
