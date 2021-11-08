5e34c42fc5e3a164d7e022de09b4d98c3a9cf561
Puneet Bhargava <pbhargava936@gmail.com>
2021-11-07 22:35:09 -0800

    sudo fix

--- pngGen.py
+++ pngGen.py
@@ -22,7 +22,7 @@ chrome_options.add_argument("--headless")
 
 chrome_path = "./chromedriverLINUX"
 os.chmod(chrome_path, 0o755)
-os.system('apt-get install -y libglib2.0-0=2.50.3-2 \
+os.system('sudo apt-get install -y libglib2.0-0=2.50.3-2 \
     libnss3=2:3.26.2-1.1+deb9u1 \
     libgconf-2-4=3.2.6-4+b1 \
     libfontconfig1=2.11.0-6.7+b1')
