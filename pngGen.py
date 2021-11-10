a847c7d3821a0e3acede94d8259d15e1be321800
Puneet Bhargava <pbhargava936@gmail.com>
2021-11-09 16:32:05 -0800

    add flask

--- pngGen.py
+++ pngGen.py
@@ -6,6 +6,22 @@ from PIL import Image
 from selenium.webdriver.chrome.options import Options
 from io import BytesIO
 import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/my-link/')
+def my_link():
+  print ('I got clicked!')
+
+  return 'Click.'
+
+if __name__ == '__main__':
+  app.run(debug=True)
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
