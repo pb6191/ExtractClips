6b023d925ebd11f7cfabef24d08a4423b9d5c696
Puneet Bhargava <pbhargava936@gmail.com>
2021-11-10 15:22:09 -0800

    enh6

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ def index():
 @app.route('/my-link/', methods=['POST'])
 def my_link():
   text = request.form['text']
-  print ('I got clicked!')
 
   chrome_options = webdriver.ChromeOptions()
   chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -29,16 +28,14 @@ def my_link():
   chrome_options.add_argument("--no-sandbox")
   driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
-  x = 5120
+  x = 3840
   y = x/16*10
   driver.set_window_size(x, y)
   driver.delete_all_cookies()
   url = "https://metatags.io/"
   driver.get(url)
-  print(driver.find_element(By.XPATH, "//div[@class = 'card-seo-facebook']").text)
-
   # make 150%
-  driver.execute_script("document.body.style.zoom = '200%'")
+  driver.execute_script("document.body.style.zoom = '150%'")
   time.sleep(5)
 
   headlines = text.split(",")
@@ -52,43 +49,15 @@ def my_link():
       im = driver.get_screenshot_as_png()
       im = Image.open(BytesIO(im))
       im1 = im.crop((x/3.71, y/2.2, x/2.105, y/1.444))
+      if (os.path.isdir('extractedImgs')):
+        shutil.rmtree("extractedImgs")
       os.mkdir("extractedImgs", 0o777)
       im1.save('extractedImgs/'+h.split('/')[-1]+'.png')
-      print("thiscod1")
+
   driver.quit()
-  print("thiscod1ss")
   shutil.make_archive("clipsArchive", 'zip', "extractedImgs")
   shutil.rmtree("extractedImgs")
   return send_file('clipsArchive.zip', as_attachment=True, download_name='clipsArchive.zip')
 
 if __name__ == '__main__':
-  print("thiscod1ss00")
   app.run(debug=True, host='0.0.0.0', port=environ.get("PORT", 5000))
-  print("thiscod1ss99")
-  
-
-# Now you can start using Selenium
-
-#chrome_options = Options()
-#chrome_options.add_argument("--headless")
-
-#chrome_path = "./chromedriverLINUX"
-#os.chmod(chrome_path, 0o755)
-
-#os.system('apt-get install -y libglib2.0-0=2.50.3-2 \
-#    libnss3=2:3.26.2-1.1+deb9u1 \
-#    libgconf-2-4=3.2.6-4+b1 \
-#    libfontconfig1=2.11.0-6.7+b1 \
-#    libgtk2.0-0:i386 \
-#    libsm6:i386')
-
-#os.system('apt-get update')
-#os.system('apt-get install wget')
-#os.system('apt-get install -y gconf-service libasound2 libatk1.0-0 libcairo2 libcups2 libfontconfig1 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libxss1 fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils default-jdk')
-#os.system('wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb')
-#os.system('dpkg -i google-chrome-stable_current_amd64.deb; apt-get -fy install')
-
-#subprocess.Popen([r"./chromedriverLINUX"])
-#executable_path=chrome_path, 
-
-#driver = webdriver.Chrome(executable_path=chrome_path, options=chrome_options)
