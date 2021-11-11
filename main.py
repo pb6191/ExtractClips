e5dcc782dcd3247957b3a20f7d9f24ea03df4245
Puneet Bhargava <pbhargava936@gmail.com>
2021-11-11 09:32:50 -0800

    fix

--- main.py
+++ main.py
@@ -80,4 +80,4 @@ def my_link():
   return render_template('index.html', message="Processing...")
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=environ.get("PORT", 5000))
+  app.run(debug=True, threaded=True, host='0.0.0.0', port=environ.get("PORT", 5000))
