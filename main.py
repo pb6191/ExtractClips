f2a4d2306b6c28eb3478ff62b4bd22d798288967
hauselin <hauselin@gmail.com>
2021-11-12 13:29:29 -0600

    Add line break to output and automatic format stuff

--- main.py
+++ main.py
@@ -1,75 +1,103 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
-from selenium.webdriver.common.by import By
-import PIL
-from PIL import Image
-from selenium.webdriver.chrome.options import Options
-from io import BytesIO
 import os
-from os import environ
-from flask import Flask,render_template,flash,request,redirect,send_file,Response,stream_with_context
 import shutil
+import time
+from io import BytesIO
+from os import environ
+
+import PIL
+from flask import (
+    Flask,
+    Response,
+    flash,
+    redirect,
+    render_template,
+    request,
+    send_file,
+    stream_with_context,
+)
+from PIL import Image
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
- return render_template('index.html', message="Idle.")
-@app.route('/my-link2/', methods=['POST'])
+    return render_template("index.html", message="Idle.")
+
+
+@app.route("/my-link2/", methods=["POST"])
 def my_link2():
-  return send_file('clipsArchive.zip', as_attachment=True, download_name='clipsArchive.zip')
+    return send_file("cards.zip", as_attachment=True, download_name="cards.zip")
 
-@app.route('/my-link/', methods=['POST'])
+
+@app.route("/my-link/", methods=["POST"])
 def my_link():
-  def generate():
-    yield 'Processing...'
-    text = request.form['text']
-    # implicit waits and parallelization
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-    driver.implicitly_wait(5)
-    x = 3840
-    y = x/16*10
-    driver.set_window_size(x, y)
-    driver.delete_all_cookies()
-    url = "https://metatags.io/"
-    driver.get(url)
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "/html/body/section[1]/input")))
-    time.sleep(5)
-    driver.execute_script("document.body.style.zoom = '150%'")
-    if (os.path.isdir('extractedImgs')):
-      shutil.rmtree("extractedImgs")
-    os.mkdir("extractedImgs", 0o777)
-    headlines = text.splitlines()
-    headlines = list(filter(None, headlines))
-    headlines = list(set(headlines))
-    for i, h in enumerate(headlines):
-      yield 'Processing...'
-      driver.find_element(By.XPATH, "/html/body/section[1]/input").clear()
-      driver.find_element(By.XPATH, "/html/body/section[1]/input").send_keys(h)
-      WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//div[@class = 'card-seo-twitter']")))
-      time.sleep(2)
-      im = driver.get_screenshot_as_png()
-      im = Image.open(BytesIO(im))
-      im1 = im.crop((x/3.71, y/2.2, x/2.105, y/1.444))
-      im1.save('extractedImgs/'+(h.split('/')[-1]).replace(".html", "")+'.png','png')
+    def generate():
+        yield "Start processing...<br>"
+        text = request.form["text"]
+        # implicit waits and parallelization
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(
+            executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options
+        )
+        driver.implicitly_wait(5)
+        x = 3840
+        y = x / 16 * 10
+        driver.set_window_size(x, y)
+        driver.delete_all_cookies()
+        url = "https://metatags.io/"
+        driver.get(url)
+        WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, "/html/body/section[1]/input"))
+        )
+        time.sleep(5)
+        driver.execute_script("document.body.style.zoom = '150%'")
+        if os.path.isdir("extractedImgs"):
+            shutil.rmtree("extractedImgs")
+        os.mkdir("extractedImgs", 0o777)
+        print(text)
+        headlines = text.splitlines()
+        headlines = list(filter(None, headlines))
+        headlines = list(set(headlines))
+        for i, h in enumerate(headlines, start=1):
+            yield f"Processing url {i}: {h}<br>"
+            driver.find_element(By.XPATH, "/html/body/section[1]/input").clear()
+            driver.find_element(By.XPATH, "/html/body/section[1]/input").send_keys(h)
+            WebDriverWait(driver, 5).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//div[@class = 'card-seo-twitter']")
+                )
+            )
+            time.sleep(2)
+            im = driver.get_screenshot_as_png()
+            im = Image.open(BytesIO(im))
+            im1 = im.crop((x / 3.71, y / 2.2, x / 2.105, y / 1.444))
+            im1.save(
+                "extractedImgs/" + (h.split("/")[-1]).replace(".html", "") + ".png",
+                "png",
+            )
 
-    driver.quit()
-    shutil.make_archive("clipsArchive", 'zip', "extractedImgs")
-    shutil.rmtree("extractedImgs")
-    yield render_template('index2.html')
-    #return send_file('clipsArchive.zip', as_attachment=True, download_name='clipsArchive.zip')
-  return Response(stream_with_context(generate()))
+        driver.quit()
+        shutil.make_archive("cards", "zip", "extractedImgs")
+        shutil.rmtree("extractedImgs")
+        yield render_template("index2.html")
+        # return send_file('cards.zip', as_attachment=True, download_name='cards.zip')
 
-if __name__ == '__main__':
- app.run(debug=True, host='0.0.0.0', port=environ.get("PORT", 5000))
+    return Response(stream_with_context(generate()))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=environ.get("PORT", 5000))
 
 
 # from selenium import webdriver
