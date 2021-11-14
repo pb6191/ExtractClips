cce6a1ef5715e03905261694e025a90b10d6eadb
Puneet Bhargava <pbhargava936@gmail.com>
2021-11-14 13:50:22 -0800

    import keys

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from flask import (
 from PIL import Image, ImageOps
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
