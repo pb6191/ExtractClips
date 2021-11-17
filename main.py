8ed3fc90f41c81d02f2f93baa5fd0cf05d55c0d9
Puneet Bhargava <pbhargava936@gmail.com>
2021-11-17 12:56:42 -0800

    fix

--- main.py
+++ main.py
@@ -131,7 +131,13 @@ def status():
         headlines = list(set(headlines))
         yield f"Processing {len(headlines)} unique urls<br><br>"
         for i, h in enumerate(headlines, start=1):
-            req = Request(h, headers={'User-Agent': 'Mozilla/5.0'})
+            hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+                'Accept-Encoding': 'none',
+                'Accept-Language': 'en-US,en;q=0.8',
+                'Connection': 'keep-alive'}
+            req = Request(h, headers=hdr)
             webpage = urlopen(req, timeout=10).read()
             soup = BeautifulSoup(webpage, "lxml")
             title = soup.find("meta", property="og:title")
