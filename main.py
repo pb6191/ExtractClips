88b9c37ee340ce92a0dbe60eaae1daccb21f2c22
Puneet Bhargava <pbhargava936@gmail.com>
2021-11-17 13:39:20 -0800

    fix

--- main.py
+++ main.py
@@ -23,6 +23,78 @@ from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
 import requests
 
+def get_title(html):
+    """Scrape page title."""
+    title = None
+    if html.title.string:
+        title = html.title.string
+    elif html.find("meta", property="og:title"):
+        title = html.find("meta", property="og:title").get('content')
+    elif html.find("meta", property="twitter:title"):
+        title = html.find("meta", property="twitter:title").get('content')
+    elif html.find("h1"):
+        title = html.find("h1").string
+    return title
+
+
+def get_description(html):
+    """Scrape page description."""
+    description = None
+    if html.find("meta", property="description"):
+        description = html.find("meta", property="description").get('content')
+    elif html.find("meta", property="og:description"):
+        description = html.find("meta", property="og:description").get('content')
+    elif html.find("meta", property="twitter:description"):
+        description = html.find("meta", property="twitter:description").get('content')
+    elif html.find("p"):
+        description = html.find("p").contents
+    return description
+
+
+def get_image(html):
+    """Scrape share image."""
+    image = None
+    if html.find("meta", property="image"):
+        image = html.find("meta", property="image").get('content')
+    elif html.find("meta", property="og:image"):
+        image = html.find("meta", property="og:image").get('content')
+    elif html.find("meta", property="twitter:image"):
+        image = html.find("meta", property="twitter:image").get('content')
+    elif html.find("img", src=True):
+        image = html.find_all("img").get('src')
+    return image
+
+
+def get_site_name(html, url):
+    """Scrape site name."""
+    if html.find("meta", property="og:site_name"):
+        site_name = html.find("meta", property="og:site_name").get('content')
+    elif html.find("meta", property='twitter:title'):
+        site_name = html.find("meta", property="twitter:title").get('content')
+    else:
+        site_name = url.split('//')[1]
+        return site_name.split('/')[0].rsplit('.')[1].capitalize()
+    return sitename
+
+
+def get_favicon(html, url):
+    """Scrape favicon."""
+    if html.find("link", attrs={"rel": "icon"}):
+        favicon = html.find("link", attrs={"rel": "icon"}).get('href')
+    elif html.find("link", attrs={"rel": "shortcut icon"}):
+        favicon = html.find("link", attrs={"rel": "shortcut icon"}).get('href')
+    else:
+        favicon = f'{url.rstrip("/")}/favicon.ico'
+    return favicon
+
+
+def get_theme_color(html):
+    """Scrape brand color."""
+    if html.find("meta", property="theme-color"):
+        color = html.find("meta", property="theme-color").get('content')
+        return color
+    return None
+    
 app = Flask(__name__)
 
 
@@ -215,74 +287,3 @@ if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=environ.get("PORT", 5000))
 
 
-def get_title(html):
-    """Scrape page title."""
-    title = None
-    if html.title.string:
-        title = html.title.string
-    elif html.find("meta", property="og:title"):
-        title = html.find("meta", property="og:title").get('content')
-    elif html.find("meta", property="twitter:title"):
-        title = html.find("meta", property="twitter:title").get('content')
-    elif html.find("h1"):
-        title = html.find("h1").string
-    return title
-
-
-def get_description(html):
-    """Scrape page description."""
-    description = None
-    if html.find("meta", property="description"):
-        description = html.find("meta", property="description").get('content')
-    elif html.find("meta", property="og:description"):
-        description = html.find("meta", property="og:description").get('content')
-    elif html.find("meta", property="twitter:description"):
-        description = html.find("meta", property="twitter:description").get('content')
-    elif html.find("p"):
-        description = html.find("p").contents
-    return description
-
-
-def get_image(html):
-    """Scrape share image."""
-    image = None
-    if html.find("meta", property="image"):
-        image = html.find("meta", property="image").get('content')
-    elif html.find("meta", property="og:image"):
-        image = html.find("meta", property="og:image").get('content')
-    elif html.find("meta", property="twitter:image"):
-        image = html.find("meta", property="twitter:image").get('content')
-    elif html.find("img", src=True):
-        image = html.find_all("img").get('src')
-    return image
-
-
-def get_site_name(html, url):
-    """Scrape site name."""
-    if html.find("meta", property="og:site_name"):
-        site_name = html.find("meta", property="og:site_name").get('content')
-    elif html.find("meta", property='twitter:title'):
-        site_name = html.find("meta", property="twitter:title").get('content')
-    else:
-        site_name = url.split('//')[1]
-        return site_name.split('/')[0].rsplit('.')[1].capitalize()
-    return sitename
-
-
-def get_favicon(html, url):
-    """Scrape favicon."""
-    if html.find("link", attrs={"rel": "icon"}):
-        favicon = html.find("link", attrs={"rel": "icon"}).get('href')
-    elif html.find("link", attrs={"rel": "shortcut icon"}):
-        favicon = html.find("link", attrs={"rel": "shortcut icon"}).get('href')
-    else:
-        favicon = f'{url.rstrip("/")}/favicon.ico'
-    return favicon
-
-
-def get_theme_color(html):
-    """Scrape brand color."""
-    if html.find("meta", property="theme-color"):
-        color = html.find("meta", property="theme-color").get('content')
-        return color
-    return None
