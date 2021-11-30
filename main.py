5a22601ba99483e67bb5ca03d02641c5cc6337d2
Puneet Bhargava <pbhargava936@gmail.com>
2021-11-29 19:06:32 -0800

    fix

--- main.py
+++ main.py
@@ -272,7 +272,7 @@ def status():
             if (validators.url(metadata["image"])):
                 Picture_request = requests.get(metadata["image"])
                 with open("tempImage.jpg", 'wb') as f3:
-                    tempContent = Image.open(BytesIO(Picture_request.content))
+                    tempContent = Image.open((Picture_request.content))
                     tempContent2 = tempContent.crop((0, 0, tempContent.width, 0.7*tempContent.height))
                     f3.write(tempContent2)
                 htmlContent2 = htmlContent2.replace("REPLACE_IMAGE", "file:///" + os.getcwd() + "//tempImage.jpg")
